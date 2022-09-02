62c475b77ea8b7985438a9fe8346465642f153b7
Swaraj Singh <76990410+Swaraj-Singh-30@users.noreply.github.com>
2022-09-02 15:11:48 +0530

    Update hello.swift

--- hello.swift
+++ hello.swift
@@ -1,1 +1,26 @@
 print("Hello Wolrd")
+
+
+import Foundation 
+import Glibc
+
+// Function to find the sum of even numbers 
+func sumEvenNum(a : Int) -> Int{
+   var sum = 0
+   print("Even numbers from 0 to \(a):")
+   for j in 0...a{
+
+      // Check if the specified number is even number or not 
+      if j % 2 == 0{
+
+         // Calculating the sum of even numbers 
+         sum += j 
+         print(j, terminator: " ,") 
+      } 
+   } 
+   return sum 
+}
+var num = 16
+
+// Calling the function and displaying the sum 
+print("\nSum of all the even numbers from 0 to \(num): ", sumEvenNum(a:num))
