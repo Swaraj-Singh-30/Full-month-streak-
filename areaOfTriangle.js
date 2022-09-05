52338b294829d1ff375dd2a5890b161320f209ee
Swaraj Singh <76990410+Swaraj-Singh-30@users.noreply.github.com>
2022-09-05 17:16:40 +0530

    DAY-05
    JS code to fins the area of a triangle

--- areaOfTriangle.js
+++ areaOfTriangle.js
@@ -1,4 +1,9 @@
 let baseValue = prompt('Enter the base of a triangle: ');
 let heightValue = prompt('Enter the height of a triangle: ');
 
+// calculate the area
 let areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+  `The area of a triangle with base ${baseValue} and height ${heightValue} is ${areaValue}`
+);
