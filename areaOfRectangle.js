0fc88f4c911fd4ffec2d7a35f8d031f377bbb8e2
Swaraj Singh <76990410+Swaraj-Singh-30@users.noreply.github.com>
2022-09-05 17:13:53 +0530

    DAY-05
    JS code to find the area of a rectangle

---
+++ areaOfRectangle.js
@@ -0,0 +1,10 @@
+var length = prompt("Enter a whole number for the length of your rectangle.");
+        var width = prompt("Enter a whole number for the width of your rectangle.");
+        function area(length, width) {
+            return length * width;
+        }
+        function perimeter(length, width) {
+            return 2*( length + width);
+        }
+        
+
