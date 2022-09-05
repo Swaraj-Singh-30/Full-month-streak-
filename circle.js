f5cc88adcbc7a379808c76636b5ecc214dbaddf2
Swaraj Singh <76990410+Swaraj-Singh-30@users.noreply.github.com>
2022-09-05 17:10:11 +0530

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,10 +1,12 @@
 function circle(radius)
 {
-    this.radius = radius
+    this.radius = radius;
+  // area method
     this.area = function () 
     {
         return Math.PI * this.radius * this.radius;
     };
+  // perimeter method
     this.perimeter = function ()
     {
         return 2*Math.PI*this.radius;
